a8003dc215ef1cfa0ebc07899d1dab2bb5fffb51
Roberto <robertorw7@gmail.com>
2023-03-02 07:41:51 -0700

    first commit

---
+++ app/src/main/java/com/example/practica6/Pelicula.kt
@@ -0,0 +1,6 @@
+package com.example.practica6
+
+data class Pelicula(var titulo: String,
+                    var imagen: Int,
+                    var header: Int,
+                    var sinopsis: String)
